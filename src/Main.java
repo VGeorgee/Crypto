6eab3c6bbdd9ae217246bc53f6dbe558e213b5be
Vgeorgee <cracken1111@gmail.com>
2019-11-20 20:55:57 +0100

    beta

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,19 @@ public class Main {
         System.out.println(e);
         System.out.println((new BigInteger("3")).compareTo(BigInteger.ONE));
 
-        */
+        // */
+        RSA r = new RSA();
+        //System.out.println(ExtendedEuclidean.compute(new BigInteger("47"), new BigInteger("252252")).X.mod(new BigInteger("252252")));
+        r.generateKeys(Prime.generatePrime(256), Prime.generatePrime(128));
+        //r.generateKeys(Prime.generatePrime(256), Prime.generatePrime(256));
+        System.out.println(r.decrypt(r.encrypt(2330)));
+        /*
         
         BigInteger definitelyNotPrime = new BigInteger("129");
         BigInteger probablyPrime = BigInteger.probablePrime(10, new Random());
         //System.out.println(MillerRabin.isPrime(probablyPrime));
         System.out.println(MillerRabin.isPrime( BigInteger.probablePrime(10, new Random())));
         System.out.println(Prime.generatePrime(20));
+         // */
     }
 }
--- src/RSA.java
+++ src/RSA.java
@@ -1,13 +1,98 @@
 
+import java.math.BigInteger;
+
+
 
 public class RSA {
+    private static final int initialBitSize = 128;
+    
+    PublicKey publicKey;
+    PrivateKey privateKey;
+    BigInteger n;
+    
+    
+    public void generateKeys(){
+        generateKeys(Prime.getPrimeFromBigInteger(initialBitSize), Prime.getPrimeFromBigInteger(initialBitSize));
+    }
+    
+    public void generateKeys(BigInteger a, BigInteger b){
+        BigInteger phiN = a.subtract(BigInteger.ONE).multiply(b.subtract(BigInteger.ONE));
+        generateKeys(a, b, Helper.getE(phiN));
+    }
+    
+    public void generateKeys(BigInteger a, BigInteger b, BigInteger e){
+        BigInteger N = a.multiply(b);
+        this.n = N;
+        BigInteger phiN = a.subtract(BigInteger.ONE).multiply(b.subtract(BigInteger.ONE));
+        
+        BigInteger d = ExtendedEuclidean.compute(phiN, e).Y.mod(phiN);
+        
+        publicKey = new PublicKey(e, N);
+        privateKey = new PrivateKey(d);
+        
+    }
+    
+    public BigInteger encrypt(int numberToEncrypt){
+        BigInteger number = new BigInteger(numberToEncrypt + "");
+        return encrypt(number);
+    }
+    
+    public BigInteger encrypt(BigInteger numberToEncrypt){
+        return numberToEncrypt.modPow(publicKey.e, publicKey.n);
+    }
+        
+    public BigInteger decrypt(BigInteger numberToDecrypt){
+        return numberToDecrypt.modPow(privateKey.d, n);
+    }
+    
+    public BigInteger decryptWithCRT(BigInteger numberToDecrypt){
+        return null;
+    }
     
 }
 
 class PublicKey{
-    
+    BigInteger e;
+    BigInteger n;
+
+    public PublicKey(BigInteger e, BigInteger n) {
+        this.e = e;
+        this.n = n;
+    }
+
+    @Override
+    public String toString() {
+        return "PublicKey{ n=" + n + ", e=" + e + " }";
+    }
+
 }
 
 class PrivateKey{
+    BigInteger d;
+
+    public PrivateKey(BigInteger d) {
+        this.d = d;
+    }
+
+    @Override
+    public String toString() {
+        return "PrivateKey{ " + "d=" + d + " }";
+    }
+    
+}
+
+
+
+class Helper{        
+    public static BigInteger getE(BigInteger phiN){
+        BigInteger two = new BigInteger("2");
+        BigInteger bi = new BigInteger("7");
+        
+        while(!ExtendedEuclidean.compute(phiN, bi).remainder.equals(BigInteger.ONE)){
+            bi = bi.add(two);
+        }
+        
+        return bi;
+    }
     
 }
